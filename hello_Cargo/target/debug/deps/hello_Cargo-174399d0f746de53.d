b44a92801894734b68a1c97c1c3be76b98cef122
Mark-v-S <Mark_Siemens@gmx.de>
2025-07-06 00:41:59 +0200

    I did just cargo run nothing else

---
+++ hello_Cargo/target/debug/deps/hello_Cargo-174399d0f746de53.d
@@ -0,0 +1,5 @@
+/home/marks/Dokumente/learningrust/hello_Cargo/target/debug/deps/hello_Cargo-174399d0f746de53.d: src/main.rs
+
+/home/marks/Dokumente/learningrust/hello_Cargo/target/debug/deps/libhello_Cargo-174399d0f746de53.rmeta: src/main.rs
+
+src/main.rs:
---
+++ hello_Cargo/target/debug/deps/hello_Cargo-ba259fde5e88e88d.d
@@ -0,0 +1,5 @@
+/home/marks/Dokumente/learningrust/hello_Cargo/target/debug/deps/hello_Cargo-ba259fde5e88e88d.d: src/main.rs
+
+/home/marks/Dokumente/learningrust/hello_Cargo/target/debug/deps/libhello_Cargo-ba259fde5e88e88d.rmeta: src/main.rs
+
+src/main.rs:
