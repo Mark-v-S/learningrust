27d70aefc39d7f75dc6fe75403d05aa961479c2a
Mark-v-S <Mark_Siemens@gmx.de>
2025-07-06 00:30:36 +0200

    i dont understand half the changes here I just changed the moin.rs to read and give back a string

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Za wardo!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {guess}");
 }
